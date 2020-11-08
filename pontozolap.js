ad387854fed1b35613f148c20ab7e878ddf5ab51
SemseyDorka <37298534+SemseyDorka@users.noreply.github.com>
2020-11-08 14:57:22 +0100

    struktúra kialakítása, képek beillesztése

--- pontozolap.js
+++ pontozolap.js
@@ -15,56 +15,56 @@ function Pontok()
     var szsz=Math.round(parseInt(Szössz)/(30*0.01))
     var gysz=Math.round(parseInt(Gyössz)/(120*0.01))
     var sz=Math.round(parseInt(Össz)/(150*0.01)) 
-    document.getElementById("Szössz").innerHTML="A szóbeli vizsga összpontszáma: "+Szössz+" ("+szsz+"%)"
-    document.getElementById("Gyössz").innerHTML="A gyakorlati vizsga összpontszáma: "+Gyössz+" ("+gysz+"%)"
-    document.getElementById("Össz").innerHTML="A vizsga összesített pontszáma: "+Össz+" ("+sz+"%)"
+    document.getElementById("Szössz").innerHTML=Szössz+" ("+szsz+"%)"
+    document.getElementById("Gyössz").innerHTML=Gyössz+" ("+gysz+"%)"
+    document.getElementById("Össz").innerHTML=Össz+" ("+sz+"%)"
 
     if  (szsz<12||gysz<12||sz<25)
     {
-        document.getElementById("jegyKözép").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Elégtelen (1)"
+        document.getElementById("jegyKözép").innerHTML=" Elégtelen (1)"
     }
     else
     {
         if (sz>=25&&sz<=39)
         {
-            document.getElementById("jegyKözép").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Elégséges (2)"
+            document.getElementById("jegyKözép").innerHTML=" Elégséges (2)"
         }
         if (sz>=40&&sz<=59)
         {
-            document.getElementById("jegyKözép").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Közepes (3)"
+            document.getElementById("jegyKözép").innerHTML=" Közepes (3)"
         }
         if (sz>=60&&sz<=79)
         {
-            document.getElementById("jegyKözép").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Jó (4)"
+            document.getElementById("jegyKözép").innerHTML=" Jó (4)"
         }
         if (sz>=80)
         {
-            document.getElementById("jegyKözép").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Jeles (5)"
+            document.getElementById("jegyKözép").innerHTML=" Jeles (5)"
         }
     }
 
 
     if  (szsz<12||gysz<12||sz<25)
     {
-        document.getElementById("jegyEmelt").innerHTML="Osztályzat emeltszintű érettségi vizsga esetén: Elégtelen (1)"
+        document.getElementById("jegyEmelt").innerHTML=" Elégtelen (1)"
     }
     else
     {
         if (sz>=25&&sz<=32)
         {
-            document.getElementById("jegyEmelt").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Elégséges (2)"
+            document.getElementById("jegyEmelt").innerHTML=" Elégséges (2)"
         }
         if (sz>=33&&sz<=46)
         {
-            document.getElementById("jegyEmelt").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Közepes (3)"
+            document.getElementById("jegyEmelt").innerHTML=" Közepes (3)"
         }
         if (sz>=47&&sz<=59)
         {
-            document.getElementById("jegyEmelt").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Jó (4)"
+            document.getElementById("jegyEmelt").innerHTML=" Jó (4)"
         }
         if (sz>=60)
         {
-            document.getElementById("jegyEmelt").innerHTML="Osztályzat középszintű érettségi vizsga esetén: Jeles (5)"
+            document.getElementById("jegyEmelt").innerHTML=" Jeles (5)"
         }
     }
 
